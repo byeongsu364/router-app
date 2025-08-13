d3308dc7e2f3b2713c30317b691a1ec79eb160e1
byeongsu364 <kbsyjysbs@gmail.com>
2025-08-13 15:52:40 +0900

    Update sns.js

--- src/data/sns.js
+++ src/data/sns.js
@@ -23,7 +23,7 @@ export const snsLinks = [
         id: 3,
         name: "profile",
         handle: "profile",
-        url: "https://fconline.nexon.com/profile/stat",
+        url: "https://fcgg.kr/%EC%A0%84%EC%A0%81%EA%B2%80%EC%83%89/%EA%B5%AC%EB%A6%AC%EC%9D%BC%EB%93%B1/%EA%B3%B5%EC%8B%9D%EA%B2%BD%EA%B8%B0",
         icon: icon3,        // 아이콘3 사용
         color: "#000000",
     },
